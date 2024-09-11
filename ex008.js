576a14de980f678b677ef2f018fc0a8fbd579d3f
Pedro de Castro Kurtz <pedrokurtz.01@outlook.com>
2024-09-11 11:52:39 -0300

    'escritorio'

---
+++ ex008.js
@@ -0,0 +1,9 @@
+var vel = 60
+
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+
+if (vel > 80) {
+    console.log('Você ultrapassou a velocidade máxima permitida. MULTADO!')
+}
+
+console.log('Dirija sempre com o cinto de segurança.')
---
+++ ex009.js
@@ -0,0 +1,8 @@
+var pais = 'Brasil'
+console.log(`Morando em ${pais}`)
+
+if (pais == 'Brasil') {
+    console.log('Você é Brasileira')
+} else {
+    console.log('Você é estrangeira')
+}
